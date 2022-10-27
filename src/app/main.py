a8b193d4617fa02574956c07e20099fdf86df3bf
Thomas <TFox95@aol.com>
2022-10-27 08:46:18 -0500

    init

---
+++ src/app/main.py
@@ -0,0 +1,29 @@
+import uvivorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from app.core.config import settings
+
+
+
+def get_application():
+    _app = FastAPI(title=settings.PROJECT_NAME)
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return _app
+
+
+app = get_application()
+
+
+if __name__ == "__main__":
+    _app = get_application()
+    uvivorn(_app, )
