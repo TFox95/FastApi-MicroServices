737cc569c36736b66fe088d51e56ca913622770d
Thomas <TFox95@aol.com>
2022-10-31 20:29:41 -0500

    removed get_db function from main.py and moved it to sql_app/database.py

--- auth/app/sql_app/database.py
+++ auth/app/sql_app/database.py
@@ -12,3 +12,10 @@ engine = create_engine(url=settings.DB_URL)
 SessionCloud = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base: DeclarativeMeta = declarative_base()
+
+def get_db():
+    db = database.SessionCloud()
+    try:
+        yield db
+    finally:
+        db.close()
