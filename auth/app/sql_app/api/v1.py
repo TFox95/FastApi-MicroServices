0eb1e5e0526f3ffb8871276e871e906875ae05d2
Thomas <TFox95@aol.com>
2022-11-02 06:17:19 -0500

    removed unused route within sql_app/api/v1 and cleaned up remaining code. also initialized app structure for auth

--- auth/app/sql_app/api/v1.py
+++ auth/app/sql_app/api/v1.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Request, Body, Header
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from sql_app.database import get_db
 from core import logging
@@ -8,23 +9,20 @@ router = APIRouter()
 
 
 @router.get("/db/conn")
-async def get_sql_app(res= JSONResponse,req = Request, db: Session= Depends(get_db)):
+async def get_sql_app(res=JSONResponse, req=Request, db: Session = Depends(get_db)):
 
     try:
-        if  db:
+        if db:
             print(db.connection())
             hellow: str = "helloworld"
             return res({"sucess": hellow}, status.HTTP_201_CREATED)
-        
+
         return res({"error": {
-            HTTPException(status.HTTP_400_BAD_REQUEST, {"token":f"Bearer {hellow}", "crazy": "Nutssafbousgjnel"})
-        }})
-    
+            HTTPException(status.HTTP_400_BAD_REQUEST)
+        }}, status.HTTP_400_BAD_REQUEST)
+
     except Exception as e:
         print(e)
-        return res({"error": "Opps, something went wrong."}, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-@router.post("/create")
-async def letmeesee(payload: dict= Body(), res= JSONResponse, db: Session= Depends(get_db)):
-    return {"success":payload}
+        return res({"error": {"Opps, something went wrong.",
+                              status.HTTP_500_INTERNAL_SERVER_ERROR}},
+                   status.HTTP_500_INTERNAL_SERVER_ERROR)
