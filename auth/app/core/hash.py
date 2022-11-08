10cd08970edf011fb38591fe92f7e55e27d05690
Thomas <TFox95@aol.com>
2022-11-08 10:13:44 -0600

    refactored if-else statements within each class based function, added salting and peppering to encode and verify, & created a dynamic variable which has the type of 'str | None' to enocde our salted and peppered key. Also added Error handling for pepper function argument.

--- auth/app/core/hash.py
+++ auth/app/core/hash.py
@@ -3,48 +3,53 @@ import hashlib
 
 class Hash():
 
-    def encode(key: str, algorithm: str = None) -> str:
+    def encode(key: str, pepper: str, algorithm: str = None) -> str:
         """
         class Based function that takes in a key and optional algorithm arguments
-        to return a hashed value string for the key that was implemented based on the
+        to return a salted and peppered hashed value string for the key that was implemented based on the
         algorithm used. Avaiable algorithms are sha3_256 & sha3_512.
 
         If no algorithm is Selected, the default algorithm of sha3_256 will
         be selected.
         """
+        salt = "bY64tISJiKBBXhr"
+        saltKeyPepper: str = f"{salt}{key}{pepper}"
+
         if type(key) is not str:
             raise Exception(
                 "value passed was not a str; argument must be a String")
+        if type(pepper) is not str:
+            raise Exception(
+                "Pepper passed was not a str; argument must be a String")
         if algorithm and type(algorithm) is not str:
             raise Exception(
                 "value passed was not Falsy or a str; Field must either be blank, Falsy, or a String")
 
         try:
-            if algorithm is str("sha3_256") or not algorithm:
-                encode = hashlib.sha3_256(key.encode())
-                hexEncoded = encode.hexdigest()
-                return hexEncoded
+            encoder: str | None = hashlib.sha3_256(saltKeyPepper.encode()).hexdigest() if algorithm is str(
+            "sha3_256") or not algorithm else hashlib.sha3_512(saltKeyPepper.encode()).hexdigest() if algorithm is str(
+                "sha3_512") else None
 
-            elif algorithm is str("sha3_512"):
-                encode = hashlib.sha3_512(key.encode())
-                hexEncoded = encode.hexdigest()
-                return hexEncoded
-
-            else:
+            if not encoder:
                 raise Exception("incorrect algorithm used. Please utilize one of the following algorithms: sha3_256 & sha3_512. Or leave algorithm field blank, which will invoke default algorithm, which is sha3_256")
+            
+            return encoder
 
         except Exception as exc:
-            return exc
+            raise exc
 
-    def verify(key: str, keyHash: str, algorithm: str = None) -> bool:
+
+    def verify(key: str, keyHash: str, pepper: str, algorithm: str = None) -> bool:
         """
         class Based function that takes in a key, a hash of a key, & optional algorithm arguments
-        to return a True or False value depending on the comparison of the two provided keys. 
-        Avaiable algorithms are sha3_256 & sha3_512.
+        to return a True or False value depending on the comparison of the  salted & peppered two 
+        provided keys. Avaiable algorithms are sha3_256 & sha3_512.
 
         If no algorithm is Selected, the default algorithm of sha3_256 will be
         be selected.
         """
+        salt = "bY64tISJiKBBXhr"
+        saltKeyPepper: str = f"{salt}{key}{pepper}"
 
         if type(key) is not str:
             raise Exception(
@@ -52,31 +57,22 @@ class Hash():
         if type(keyHash) is not str:
             raise Exception(
                 "keyHash passed wasn't a str; argument must be a String")
+        if type(pepper) is not str:
+            raise Exception(
+                "Pepper passed wasn't a str; argument must be a String")
         if algorithm and type(algorithm) is not str:
             raise Exception(
                 "value passed was not None or a str; Field must either be blank, or a String")
 
         try:
-            if algorithm is str("sha3_256") or not algorithm:
-                encode = hashlib.sha3_256(key.encode())
-                hexEncoded = encode.hexdigest()
+            encoder:hashlib.sha3_256 = hashlib.sha3_256(saltKeyPepper.encode()).hexdigest() if algorithm is str(
+            "sha3_256") or not algorithm else hashlib.sha3_512(saltKeyPepper.encode()).hexdigest() if algorithm is str(
+                "sha3_512") else None
 
-                if hexEncoded != keyHash:
-                    return False
-
-                return True
-
-            elif algorithm is str("sha3_512"):
-                encode = hashlib.sha3_512(key.encode())
-                hexEncoded = encode.hexdigest()
-
-                if hexEncoded != keyHash:
-                    return False
-
-                return True
-
-            else:
-                raise Exception("incorrect algorithm used. Please utilize one of the following algorithms: sha3_256 & sha3_512. Or leave algorithm field blank, which will invoke default algorithim, which is sha3_256")
-
+            if not encoder or encoder != keyHash:
+                return False
+            
+            return True
+                
         except Exception as exc:
             raise exc
