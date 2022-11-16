82287e90ccc222ad4b775dbcd48723988f617dc9
Thomas <TFox95@aol.com>
2022-11-16 09:29:40 -0600

    corrected Mislabled each print String within each function

--- auth/app/core/logging.py
+++ auth/app/core/logging.py
@@ -15,20 +15,20 @@ def ServerINFO(NAMESPACE: str, Message: str, obj=None):
 
 def ServerWARNING(NAMESPACE: str, Message: str, obj=None):
     if obj:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}, [object] {obj}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [WARNING] {Message}, [object] {obj}")
     else:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [WARNING] {Message}")
 
 
 def ServerERROR(NAMESPACE: str, Message: str, obj=None):
     if obj:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}, [object] {obj}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [ERROR] {Message}, [object] {obj}")
     else:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [ERROR] {Message}")
 
 
 def ServerDEBUG(NAMESPACE: str, Message: str, obj=None):
     if obj:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}, [object] {obj}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [DEBUG] {Message}, [object] {obj}")
     else:
-        print(f"[{ServerDateTime()}] [{NAMESPACE}] [INFO] {Message}")
+        print(f"[{ServerDateTime()}] [{NAMESPACE}] [DEBUG] {Message}")
