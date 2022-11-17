ea21349b65afd1237262d80f98b4af20b13cbebd
Thomas <TFox95@aol.com>
2022-11-17 17:43:57 -0600

    added error handling logic using if statements to check if the Email or Username is already within the mysql database. If so will return 409 conflit HTTP status, also checks if the password and confirm passwords are the same if not also return a conflit HTTP status

--- auth/app/auth/api/routes.py
+++ auth/app/auth/api/routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.responses import JSONResponse
+
 from sql_app.database import get_db
 from sqlalchemy.orm import Session
+
 from auth import schemas, models, crud
-
-
 
 router = APIRouter(
     prefix="/auth",
@@ -17,12 +17,25 @@ def get_auth():
     return "auth app created!"
 
 
-@router.post("/create")
-def createUser(request: schemas.UserCreate, db: Session = Depends(get_db)):
+@router.post("/create", response_model_exclude={"password", "pk", "isAdmin"})
+def Register(request: schemas.UserCreate, db: Session = Depends(get_db)):
     
-    if request.psw !=  request.re_psw:
+    dbEmailQuery:User = db.query(User).filter(User.email == request.email).first() or None
+    dbUsernameQuery: User = db.query(User).filter(User.username == request.username).first() or None
+
+    if dbEmailQuery or dbUsernameQuery or (request.psw != request.re_psw):
+        if dbEmailQuery:
+            return JSONResponse(
+                {"error": f"Email, {dbEmailQuery.email}, is already in use"}, status_code=status.HTTP_409_CONFLICT
+            )
+        
+        if dbUsernameQuery:
+            return JSONResponse(
+                {"error": f"Username, {dbUsernameQuery.username}, is already in use"}, status_code=status.HTTP_409_CONFLICT
+            )
+        
         return JSONResponse(
-            {"error": "Passwords don't match"}, status_code=status.HTTP_400_BAD_REQUEST
+            {"error": "Passwords don't match; Passwords must be the same"}, status_code=status.HTTP_409_CONFLICT
         )
 
     _user = crud.UserCRUD.create_User(db, request)
@@ -30,5 +43,7 @@ def createUser(request: schemas.UserCreate, db: Session = Depends(get_db)):
     if not _user:
         return "error"
 
-    return _user
+    return JSONResponse({
+        "success": {_user}
+    }, status.HTTP_201_CREATED)
 
