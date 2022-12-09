46374f78349fab271ae2ca9ef7ae2cb9828c9f47
Thomas <TFox95@aol.com>
2022-12-09 16:15:23 -0600

    added NAMESPACE, User, and Oauth2_scheme global variables, within Register function changes last method from first() to scalar() because there should be no duplicates because of sections being unique, also changed Register func. Response to only include the username and user created string, lastly created Login func. logic that discerns if the passed 'username' is either a username or an email and validates the password.

--- auth/app/auth/api/routes.py
+++ auth/app/auth/api/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Request, Form
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 
 from sql_app.database import get_db
@@ -6,10 +7,16 @@ from sqlalchemy.orm import Session
 
 from auth import schemas, models, crud
 
+NAMESPACE = f"Auth Routes"
+
 router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
+
+User = models.User
+
+Oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
 @router.get("/")
@@ -20,30 +27,37 @@ def get_auth():
 @router.post("/create", response_model_exclude={"password", "pk", "isAdmin"})
 def Register(request: schemas.UserCreate, db: Session = Depends(get_db)):
     
-    dbEmailQuery:User = db.query(User).filter(User.email == request.email).first() or None
-    dbUsernameQuery: User = db.query(User).filter(User.username == request.username).first() or None
-
+    dbEmailQuery:User = db.query(User).filter(User.email == request.email).scalar() or None
+    dbUsernameQuery: User = db.query(User).filter(User.username == request.username).scalar() or None
+    
     if dbEmailQuery or dbUsernameQuery or (request.psw != request.re_psw):
         if dbEmailQuery:
             return JSONResponse(
                 {"error": f"Email, {dbEmailQuery.email}, is already in use"}, status_code=status.HTTP_409_CONFLICT
             )
-        
         if dbUsernameQuery:
             return JSONResponse(
                 {"error": f"Username, {dbUsernameQuery.username}, is already in use"}, status_code=status.HTTP_409_CONFLICT
             )
-        
         return JSONResponse(
             {"error": "Passwords don't match; Passwords must be the same"}, status_code=status.HTTP_409_CONFLICT
         )
+    
 
     _user = crud.UserCRUD.create_User(db, request)
 
-    if not _user:
-        return "error"
-
     return JSONResponse({
-        "success": {_user}
+        "success": f"User, {_user.username}, has been created!"
     }, status.HTTP_201_CREATED)
 
+@router.post("/token")
+def login(formData: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+    This function only recieves data as Form data returns a jwt token
+    """
+    
+    user: User = crud.UserCRUD.retrieve_User(db, email=formData.username) if "@" in str(formData.username
+        ) else crud.UserCRUD.retrieve_User(db, username=formData.username)
+    checkPassword = crud.AuthHandler().verify_password(psw=formData.password, hashed_psw=user.password)
+    
+    return checkPassword
