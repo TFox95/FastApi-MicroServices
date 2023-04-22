cf0bb7294b3fb5f454b3eb200bccbb11dd65c256
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-04-21 22:12:24 -0500

    Update schemas.py

--- auth/app/auth/schemas.py
+++ auth/app/auth/schemas.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel as Base, EmailStr
 from datetime import datetime
+from typing import Union
 
 
 class AddressBase(Base):
-    streetNumber: int | None 
-    streetName: str | None = None
-    aptNumber: str | None = None
+    streetNumber: Union[int , None] 
+    streetName: Union[str , None] = None
+    aptNumber: Union[str , None] = None
 
-    ZipCode: int | None
-    city: str | None = None
-    state: str | None = None
+    ZipCode: Union[int , None]
+    city: Union[str , None] = None
+    state: Union[str , None] = None
 
 
 class AddressCreate:
@@ -17,12 +18,12 @@ class AddressCreate:
 
 
 class ProfileBase(Base):
-    firstName: str | None = None
-    lastName: str | None = None
+    firstName: Union[str , None] = None
+    lastName: Union[str , None] = None
     addresses: list[AddressBase] = []
 
-    stripe_Cust_ID: str | None = None
-    One_click_Purchasing: bool | None = None
+    stripe_Cust_ID: Union[str , None] = None
+    One_click_Purchasing: Union[bool , None] = None
 
 
 class ProfileCreate(ProfileBase):
@@ -37,14 +38,14 @@ class UserLogin(Base):
 class UserBase(Base):
     email: EmailStr
     username: str
-    UUID: str | None = None
-    pk: int | None
+    UUID: Union[str , None] = None
+    pk: Union[int , None]
     
     verified: bool = False
     isAdmin: bool = False
 
-    dateJoined: datetime | str | None
-    lastLogin: datetime | str | None = None
+    dateJoined: Union[datetime , str , None]
+    lastLogin: Union[datetime , str , None] = None
 
     class Config:
         orm_mode = True
